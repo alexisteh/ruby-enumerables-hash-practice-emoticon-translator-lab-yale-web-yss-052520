d17ad2c85d1cfd1d5b25821df03e1c7f76c09d7d
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-12 23:51:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(filepath) 
+  nh = YAML.load_file(filepath) 
+  new = {}
+  nh.each do |key,val| 
+    new[key] = { :english => val[0], :japanese => val[1] }
+  end 
+  puts new
+  new 
 end
 
-def get_japanese_emoticon
-  # code goes here
+load_library("lib/emoticons.yml")
+
+def get_japanese_emoticon(filepath,emo) 
+  1 
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(filepath, emo)
+  hash = load_library(filepath) 
+  hash.each do |key,val| 
+    if val[:japanese] == emo 
+      return key 
+    end 
+  end 
+  return "Sorry, that emoticon was not found"
 end
+
+get_english_meaning("./lib/emoticons.yml", "(Ｔ▽Ｔ)")
